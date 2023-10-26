32e2dc14e35c11137e712a4fb48896ec6e52e1d1
Onkar Karale <onkarkarale4@gmail.com>
2023-10-26 15:53:55 +0530

    Hoisting in JavaScript (variables & functions)

--- EPISODE-2/index.js
+++ EPISODE-2/index.js
@@ -1,7 +1,26 @@
+// Without Hoisting
 var x = 7
+
+function myName () {
+  console.log('Omkar Karale')
+}
+
+myName()
+console.log(x)
+
+var height = 180
+console.log(height); // -> 180
+console.log(weight); // -> undefined
+var weight = 90
+console.log(weight)
+
+// Hoisting
+console.log(y)
+var y = 9
+var x = 7
+console.log(x)
 getName() // Calling before declaration
 setName() // Calling before declaration
-console.log(x)
 
 function getName () { // Declaring
   console.log('Namaste Javascript')
@@ -11,4 +30,4 @@ function setName () {
   console.log('Namaste React')
 }
 
-// In the above program, the function getName() and setName() and variable x are called before declaring it and the program shows the output. This is due to hoisting.
+// In the above program, the function getName() , setName() and variable x are called before declaring it however the program shows the output. This is due to hoisting.
