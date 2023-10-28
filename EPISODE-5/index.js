38b7c0950e8e52cd3c79799db6d11b8d78d31ac9
Onkar Karale <onkarkarale4@gmail.com>
2023-10-28 18:11:36 +0530

    Shortest JS Program, window & this keyword

--- EPISODE-5/index.js
+++ EPISODE-5/index.js
@@ -1,4 +1,13 @@
-console.log(x) // undefined
-var x = 25
-console.log(x) // 25
-console.log(a); // Uncaught ReferenceError: a is not defined
+var x = 10
+// Global Space
+function b () {
+  var a = 20
+  console.log(a)
+// Local Space
+}
+
+console.log(x) // 10
+console.log(this.x) // 10
+console.log(window.x) // 10
+
+// So here x, this.x and window.x all are refering to the same place in the memory space.
