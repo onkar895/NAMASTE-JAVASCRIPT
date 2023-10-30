71082f7b410c6b718d70bb8b034f3b2891eefffe
Onkar Karale <onkarkarale4@gmail.com>
2023-10-30 23:30:27 +0530

    Block Scope & Shadowing in JS

--- EPISODE-9/index.js
+++ EPISODE-9/index.js
@@ -28,14 +28,14 @@ console.log(c) // ReferenceError: c is not defined
 console.log(y) // ReferenceError: y is not defined
 
 // Shadowing : We can declare the global variable and a local variable of the same name.
-var a = 100
+var m = 100
 {
-  var a = 10 // same name as global var
-  let b = 20
-  const c = 30
-  console.log(a) // 10
-  console.log(b) // 20
-  console.log(c) // 30
+  var m = 10 // same name as global var
+  let n = 20
+  const p = 30
+  console.log(m) // 10
+  console.log(n) // 20
+  console.log(p) // 30
 }
-console.log(a) // 10 
+console.log(m) // 10 
 // Instead of the 100 we were expecting.So block "a" modified value of global "a" as well.In console, only b and c are in block space.a initially is in global space(a = 100), and when a = 10 line is run, a is not created in block space, but replaces 100 with 10 in global space itself.
