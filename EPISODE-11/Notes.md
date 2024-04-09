aa4c30473d0c9f9e5d69a27e8d10bb04bb81d143
Onkar Karale <onkarkarale4@gmail.com>
2024-04-09 11:45:19 +0530

    setTimeout + Closures Interview Question

--- EPISODE-11/Notes.md
+++ EPISODE-11/Notes.md
@@ -26,12 +26,12 @@ x();
 
 ```js
 function x() {
-for (var i = 1; i <= 5; i++) {
-setTimeout(function () {
-console.log(i);
-}, i \* 1000);
-}
-console.log('Namaste Javascript');
+    for (var i = 1; i <= 5; i++) {
+        setTimeout(function () {
+            console.log(i);
+        }, i * 1000);
+    }
+    console.log("Namaste Javascript");
 }
 x();
 // Output:
@@ -55,16 +55,16 @@ x();
 
 ```js
 function x() {
-for (var i = 1; i <= 5; i++) {
-function close(i) {
-setTimeout(function () {
-console.log(i);
-}, i \* 1000);
-// put the setT function inside new function close()
-}
-close(i); // everytime you call close(i) it creates new copy of i. Only this time, it is with var itself!
-}
-console.log('Namaste Javascript');
+    for (var i = 1; i <= 5; i++) {
+        function close(i) {
+            setTimeout(function () {
+                console.log(i);
+            }, i * 1000);
+            // put the setT function inside new function close()
+        }
+        close(i); // everytime you call close(i) it creates new copy of i. Only this time, it is with var itself!
+    }
+    console.log("Namaste Javascript");
 }
 x();
 ```
--- EPISODE-11/index.js
+++ EPISODE-11/index.js
@@ -1,16 +1,26 @@
-const x = () => {
-  var i = 1
-  setTimeout(() => {
-    console.log(i)
-  }, 3000)
-  console.log('Namaste Javascript')
-}
-x()
-// So, this setTimeout takes the callback function and attaches the timer of 3000 milliseconds to it.
+// const x = () => {
+//   var i = 1
+//   setTimeout(() => {
+//     console.log(i)
+//   }, 3000)
+//   console.log('Namaste Javascript')
+// }
+// x()
+// So, this setTimeout takes the callback function and attaches the timer of 3000 milliseconds to it. and stores it somewhere.
 // and once the timer expires, it takes that function and puts it again to the call stack and runs it, that's how setTimeout work.
 
+// function y () {
+//   for (var i = 1; i <= 5; i++) {
+//     setTimeout(function () {
+//       console.log(i)
+//     }, i * 1000)
+//   }
+//   console.log('Namaste Javascript')
+// }
+// y()
+
 function y () {
-  for (var i = 1; i <= 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     setTimeout(function () {
       console.log(i)
     }, i * 1000)
@@ -19,28 +29,27 @@ function y () {
 }
 y()
 
-function z () {
-  for (var i = 1; i <= 5; i++) {
-    function close (i) {
-      setTimeout(function () {
-        console.log(i)
-      }, i * 1000)
-    // put the setT function inside new function close()
-    }
-    close(i); // everytime you call close(i) it creates new copy of i. Only this time, it is with var itself!
-  }
-  console.log('Namaste Javascript')
-}
-z()
+// function z () {
+//   for (var i = 1; i <= 5; i++) {
+//     function close (i) {
+//       setTimeout(function () {
+//         console.log(i)
+//       }, i * 1000)
+// put the setT function inside new function close()
+//     }
+//     close(i); // everytime you call close(i) it creates new copy of i. Only this time, it is with var itself!
+//   }
+//   console.log('Namaste Javascript')
+// }
+// z()
 
 // Real time example of closure :
-const clickHandler = (color) => {
-  const newFunc = () => {
-    document.body.style.backgroundColor = `${color}`
-  }
+// const clickHandler = (color) => {
+//   const newFunc = () => {
+//     document.body.style.backgroundColor = `${color}`
+//   }
+//   return newFunc
+// }
 
-  return newFunc
-}
-
-document.getElementById('orange').onclick = clickHandler('orange')
-document.getElementById('purple').onclick = clickHandler('purple')
+// document.getElementById('orange').onclick = clickHandler('orange')
+// document.getElementById('purple').onclick = clickHandler('purple')
