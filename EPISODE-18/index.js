39589969ce0613b7dd631bb1aeca78cc292ae9f4
Onkar Karale <onkarkarale4@gmail.com>
2024-04-24 20:10:51 +0530

    Higher-Order Functions ft. Functional Programming

--- EPISODE-18/index.js
+++ EPISODE-18/index.js
@@ -10,3 +10,31 @@ function y (x) {
 y(x) // Hello
 // So here,y is a higher order function.
 // and x is a callback function.
+
+// Let's try to understand how we should approach solution in interview. I have an array of radius and I have to calculate area using these radius and store in an array.
+
+// First Approach:
+
+const radius = [3, 2, 4, 5]
+
+const calculateArea = function (radius) {
+  const output = []
+  for (let i = 0; i < radius.length; i++) {
+    output.push(Math.PI * radius[i] * radius[i])
+  }
+  return output
+}
+
+console.log(calculateArea(radius))
+
+// The above solution works perfectly fine but what if we have now requirement to calculate array of circumference. Code now be like
+
+const radius1 = [1, 2, 3, 4]
+const calculateCircumference = function (radius1) {
+  const output = []
+  for (let i = 0; i < radius1.length; i++) {
+    output.push(2 * Math.PI * radius1[i])
+  }
+  return output
+}
+console.log(calculateCircumference(radius1))
