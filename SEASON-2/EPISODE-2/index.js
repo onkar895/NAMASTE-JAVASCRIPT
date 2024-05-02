b3064a3f08c9df61d59a17bb7d9710c7e5e05d89
Onkar Karale <onkarkarale4@gmail.com>
2024-05-02 23:46:39 +0530

    Namaste JavaScript season 2 - Promises

--- SEASON-2/EPISODE-2/index.js
+++ SEASON-2/EPISODE-2/index.js
@@ -13,4 +13,58 @@ createOrder(cart, function () {
 })
 // Above there is the issue of `Inversion of Control`
 
+// Que: Question is how we will get to know response is ready?
+// Ans: So, we will attach a callback function to the promise object using then to get triggered automatically when result is ready.
+const cart1 = ['shoes', 'pants', 'kurta']
 
+const promiseRef = createOrder(cart1)
+// this promiseRef has access to `then`
+
+// {data: undefined}
+// Initially it will be undefined so below code won't trigger
+// After some time, when execution has finished and promiseRef has the data then automatically the below line will get triggered.
+
+promiseRef.then(function () {
+  proceedToPayment(orderId)
+})
+
+// Callback Hell Example
+createOrder(cart, function (orderId) {
+  proceedToPayment(orderId, function (paymentInf) {
+    showOrderSummary(paymentInf, function (balance) {
+      updateWalletBalance(balance)
+    })
+  })
+})
+// And now above code is expanding horizontally and this is called pyramid of doom.
+// Callback hell is ugly and hard to maintain.
+
+// 💡 Promise fixes this issue too using `Promise Chaining`
+// Example Below is a Promise Chaining
+createOrder(cart)
+  .then(function (orderId) {
+    proceedToPayment(orderId)
+  })
+  .then(function (paymentInf) {
+    showOrderSummary(paymentInf)
+  })
+  .then(function (balance) {
+    updateWalletBalance(balance)
+  })
+
+// ⚠️ Common PitFall
+// We forget to return promise in Promise Chaining
+// The idea is promise/data returned from one .then become data for next .then
+// So,
+createOrder(cart)
+  .then(function (orderId) {
+    return proceedToPayment(orderId)
+  })
+  .then(function (paymentInf) {
+    return showOrderSummary(paymentInf)
+  })
+  .then(function (balance) {
+    return updateWalletBalance(balance)
+  })
+
+  // To improve readability you can use arrow function instead of regular function
