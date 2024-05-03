fac696b1370a3f2ac2b8220e1c30a38214ce6f26
Onkar Karale <onkarkarale4@gmail.com>
2024-05-03 11:49:47 +0530

    Namaste JavaScript season 2 - Promises

--- SEASON-2/EPISODE-2/index.js
+++ SEASON-2/EPISODE-2/index.js
@@ -1,33 +1,5 @@
 // Promises :
 
-const cart = ['shoes', 'pants', 'kurta']
-
-// Below two functions are asynchronous and dependent on each other
-const orderId = createOrder(cart)
-proceedToPayment(orderId)
-
-// with Callback (Before Promise)
-// Below here, it is the responsibility of createOrder function to first create the order then call the callback function
-createOrder(cart, function () {
-  proceedToPayment(orderId)
-})
-// Above there is the issue of `Inversion of Control`
-
-// Que: Question is how we will get to know response is ready?
-// Ans: So, we will attach a callback function to the promise object using then to get triggered automatically when result is ready.
-const cart1 = ['shoes', 'pants', 'kurta']
-
-const promiseRef = createOrder(cart1)
-// this promiseRef has access to `then`
-
-// {data: undefined}
-// Initially it will be undefined so below code won't trigger
-// After some time, when execution has finished and promiseRef has the data then automatically the below line will get triggered.
-
-promiseRef.then(function () {
-  proceedToPayment(orderId)
-})
-
 // Callback Hell Example
 createOrder(cart, function (orderId) {
   proceedToPayment(orderId, function (paymentInf) {
@@ -39,7 +11,7 @@ createOrder(cart, function (orderId) {
 // And now above code is expanding horizontally and this is called pyramid of doom.
 // Callback hell is ugly and hard to maintain.
 
-// 💡 Promise fixes this issue too using `Promise Chaining`
+// 💡 Promise fixes this issue too using `Promise Chaining` :
 // Example Below is a Promise Chaining
 createOrder(cart)
   .then(function (orderId) {
