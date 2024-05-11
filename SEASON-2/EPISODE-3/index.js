54c79f0b5e7b01eb65ed6a2683fddd3f6ac54d9b
Onkar Karale <onkarkarale4@gmail.com>
2024-05-11 17:01:55 +0530

    Creating a Promise, Chaining & Error Handling

--- SEASON-2/EPISODE-3/index.js
+++ SEASON-2/EPISODE-3/index.js
@@ -1,11 +1,38 @@
+// Creating a Promise, Chaining & Error Handling :
+
 const cart = ['shoes', 'pants', 'kurta']
 
 const promise = createOrder(cart)
+console.log(promise)
 
 promise.then(function (orderId) {
-  proceedToPayment(orderId)
+  console.log(orderId)
+// proceedToPayment(orderId)
+}).catch(function (err) {
+  console.log(err.message)
 })
 
 function createOrder (cart) {
-  const promise = new Promise(function (resolve, reject) {})
+  const promise = new Promise(function (resolve, reject) {
+    // createOrder
+    // ValidateCart
+    // orderId
+    if (!validateCart(cart)) {
+      const err = new Error('Cart is not valid')
+      reject(err)
+    }
+    // Logic for createOrder
+    const orderId = '12345'
+    if (orderId) {
+      setTimeout(() => {
+        resolve(orderId)
+      }, 5000)
+    }
+  })
+
+  return promise
+}
+
+function validateCart (cart) {
+  return false
 }
